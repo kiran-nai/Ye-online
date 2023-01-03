b1f3a24c11e96b19a17ed329f576557c74077eff
kiran-nai <108095331+kiran-nai@users.noreply.github.com>
2023-01-03 17:36:59 +0000

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,35 +1,40 @@
 <?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-require "vendor/autoload.php";
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+$recipient = "example@gmail.com";
 
-$mail = new PHPMailer(true);
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+echo'
 
-$mail->isSMTP();
-$mail->SMTPAuth = true;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        
+    </div>
+</body>
+</html>
 
-$mail->Host = "smtp.example.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
 
-$mail->Username = "you@example.com";
-$mail->Password = "password";
 
-$mail->setFrom($email, $name);
-$mail->addAddress("dave@example.com", "Dave");
+';
 
-$mail->Subject = $subject;
-$mail->Body = $message;
 
-$mail->send();
-
-header("Location: sent.html");
+?>

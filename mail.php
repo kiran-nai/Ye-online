8e0f6e3d19fb0c31edd770d88b814342968f2a21
kiran-nai <108095331+kiran-nai@users.noreply.github.com>
2023-01-03 15:29:15 +0000

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,40 +1,35 @@
 <?php
+$to = "somebody@example.com, somebodyelse@example.com";
+$subject = "HTML email";
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-
-$mailheader = "From:".$name."<".$email.">\r\n";
-
-$recipient = "example@gmail.com";
-
-mail($recipient, $subject, $message, $mailheader) or die("Error!");
-
-echo'
-
-<!DOCTYPE html>
-<html lang="en">
+$message = "
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact form</title>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+<title>HTML email</title>
 </head>
 <body>
-    <div class="container">
-        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        
-    </div>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
 </body>
 </html>
+";
 
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
 
-';
-
-
+mail($to,$subject,$message,$headers);
 ?>
